a75f86441d3de86fd6b548807a30ebb17603c0db
scott <mic1028002@gmail.com>
2019-01-24 19:51:30 +0800

    fix bugs

--- silent-refresh-by-iframe-with-js-sample/rollup.config.js
+++ silent-refresh-by-iframe-with-js-sample/rollup.config.js
@@ -1,9 +1,10 @@
 export default {
   input: 'src/main.js',
   output: {
-    name: 'initTokenRefresher',  // such as: [ $ => window.$, jQuery => window.jQuery ]
+    name: 'SilentRefresher',  // such as: [ $ => window.$, jQuery => window.jQuery ]
     file: 'dist.js',
     format: 'iife',  // only for the format of platform you can execute
+    sourcemap: true,
   },
   // avaliable when append "-w"
   watch: {
